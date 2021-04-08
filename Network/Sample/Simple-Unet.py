c3bec191b16773d1be44b86afbb3044741806bad
trkdmtr <trkdmtr@gmail.com>
2021-04-08 18:44:15 +0300

    Добавлен пример сегментирующей сети

---
+++ Network/Sample/Simple-Unet.py
@@ -0,0 +1,238 @@
+import numpy as np 
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms as T
+import torchvision
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from tqdm.notebook import tqdm
+
+from PIL import Image
+from PIL.ImageOps import scale
+
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3)
+        self.relu  = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3)
+    
+    def forward(self, x):
+        return self.conv2(self.relu(self.conv1(x)))
+
+
+class Encoder(nn.Module):
+    def __init__(self, chs=(3,64,128,256,512,1024)):
+        super().__init__()
+        self.enc_blocks = nn.ModuleList([Block(chs[i], chs[i+1]) for i in range(len(chs)-1)])
+        self.pool       = nn.MaxPool2d(2)
+    
+    def forward(self, x):
+        ftrs = []
+        for block in self.enc_blocks:
+            x = block(x)
+            ftrs.append(x)
+            x = self.pool(x)
+        return ftrs
+
+
+class Decoder(nn.Module):
+    def __init__(self, chs=(1024, 512, 256, 128, 64)):
+        super().__init__()
+        self.chs         = chs
+        self.upconvs    = nn.ModuleList([nn.ConvTranspose2d(chs[i], chs[i+1], 2, 2) for i in range(len(chs)-1)])
+        self.dec_blocks = nn.ModuleList([Block(chs[i], chs[i+1]) for i in range(len(chs)-1)]) 
+        
+    def forward(self, x, encoder_features):
+        for i in range(len(self.chs)-1):
+            x        = self.upconvs[i](x)
+            enc_ftrs = self.crop(encoder_features[i], x)
+            x        = torch.cat([x, enc_ftrs], dim=1)
+            x        = self.dec_blocks[i](x)
+        return x
+    
+    def crop(self, enc_ftrs, x):
+        _, _, H, W = x.shape
+        enc_ftrs   = torchvision.transforms.CenterCrop([H, W])(enc_ftrs)
+        return enc_ftrs
+
+
+class UNet(nn.Module):
+    def __init__(self, enc_chs=(3,64,128,256,512,1024), dec_chs=(1024, 512, 256, 128, 64), num_class=1, retain_dim=False, out_sz=(572,572)):
+        super().__init__()
+        self.encoder     = Encoder(enc_chs)
+        self.decoder     = Decoder(dec_chs)
+        self.head        = nn.Conv2d(dec_chs[-1], num_class, 1)
+        self.retain_dim  = retain_dim
+        if retain_dim:
+            self.out_sz = out_sz
+
+    def forward(self, x):
+        enc_ftrs = self.encoder(x)
+        out      = self.decoder(enc_ftrs[::-1][0], enc_ftrs[::-1][1:])
+        out      = self.head(out)
+        if self.retain_dim:
+            out = F.interpolate(out, self.out_sz)
+        return out
+        
+        
+def get_names():
+    names = []
+    for _,_,files in os.walk(IMG_PATH):
+        for file in files:
+            names.append(file.split('.')[0])
+    return pd.DataFrame({'id':names}, index = np.arange(len(names)))
+    
+    
+def tensor_to_pillow(t):
+    """
+    Transforms a PyTorch tensor with dimensions suitable for network
+    to a Pillow image to show it
+    """
+    mode = None
+    sh = t.shape
+    if sh[1] == 1:
+        mode = 'L'
+        t1 = t.reshape(sh[2],sh[3])
+    else:    
+        t1 = t.reshape(sh[1],sh[2],sh[3])
+        t1 = t1.permute(1,2,0)
+    t1 = t1.detach().numpy()
+    t1 = t1*255
+    img = Image.fromarray(np.uint8(t1), mode)
+    return img 
+    
+    
+class DroneDataset(Dataset):
+    def __init__(self,img_path,mask_path, sample_ids):
+        self.img_path = img_path
+        self.mask_path = mask_path
+        self.sample_ids = sample_ids
+        
+    def __len__(self):
+        return len(self.sample_ids)
+    
+    def __getitem__(self,Id):
+        img = Image.open(self.img_path+self.sample_ids[Id]+'.jpg')
+        img = ImageOps.scale(img,0.25,Image.NEAREST)
+        img = T.functional.to_tensor(img)
+        
+        mask = Image.open(self.mask_path+self.sample_ids[Id]+'.png')
+        mask = ImageOps.scale(mask,0.25,Image.NEAREST)
+        mask = np.array(mask)
+        mask = torch.from_numpy(mask).long()
+        
+        return img,mask
+        
+        
+def pixel_accuracy(output, mask):
+    with torch.no_grad():
+        output = torch.argmax(F.softmax(output, dim=1), dim=1)
+        correct = torch.eq(output, mask).int()
+        accuracy = float(correct.sum()) / float(correct.numel())
+    return accuracy
+
+def mIoU(output, mask, n_classes, smooth=1e-10):
+    with torch.no_grad():
+        output = F.softmax(output, dim=1)
+        output = torch.argmax(output, dim=1)
+        output = output.reshape(-1)
+        mask = mask.reshape(-1)
+
+        iou_per_class = []
+        for c in range(0, n_classes): #loop per pixel class
+            true_class = output == c
+            true_label = mask == c
+
+            if true_label.long().sum().item() == 0: #no exist label in this loop
+                iou_per_class.append(np.nan)
+            else:
+                intersect = torch.logical_and(true_class, true_label).sum().float().item()
+                union = torch.logical_or(true_class, true_label).sum().float().item()
+
+                iou = (intersect + smooth) / (union +smooth)
+                iou_per_class.append(iou)
+        return np.nanmean(iou_per_class)
+        
+        
+def fit(epochs, model, train_loader, val_loader, criterion, optimizer):
+    torch.cuda.empty_cache()
+    train_losses = []
+    test_losses = []
+    val_iou = []; val_acc = []
+    train_iou = []; train_acc = []
+    min_loss = np.inf
+
+    model.to(device)
+    #print(torch.cuda.memory_allocated())
+    for e in range(epochs):
+        running_loss = 0
+        iou_score = 0
+        accuracy = 0
+        #training loop
+        model.train()
+        for i, data in enumerate(tqdm(train_loader)):
+            #training phase
+            image_tiles, mask_tiles = data
+            image = image_tiles.to(device)
+            #print(torch.cuda.memory_allocated())
+            mask = mask_tiles.to(device)
+            #print(torch.cuda.memory_allocated())
+            #forward
+            output = model(image)
+            loss = criterion(output, mask)
+            #evaluation metrics
+            iou_score += mIoU(output, mask, nc)
+            accuracy += pixel_accuracy(output, mask)
+            #backward
+            loss.backward()
+            optimizer.step() #update weight          
+            optimizer.zero_grad() #reset gradient
+            running_loss += loss.item()
+            
+        else:
+            model.eval()
+            test_loss = 0
+            test_accuracy = 0
+            val_iou_score = 0
+            #validation loop
+            with torch.no_grad():
+                for data in tqdm(val_loader):
+                    #reshape to 9 patches from single image, delete batch size
+                    image_tiles, mask_tiles = data
+                    image = image_tiles.to(device); mask = mask_tiles.to(device);
+                    output = model(image)
+                    #evaluation metrics
+                    val_iou_score +=  mIoU(output, mask, nc)
+                    test_accuracy += pixel_accuracy(output, mask)
+                    #loss
+                    loss = criterion(output, mask)                                  
+                    test_loss += loss.item()
+            
+            #calculatio mean for each batch
+            train_losses.append(running_loss/len(train_loader))
+            test_losses.append(test_loss/len(val_loader))
+            
+            #iou
+            val_iou.append(val_iou_score/len(val_loader))
+            train_iou.append(iou_score/len(train_loader))
+            train_acc.append(accuracy/len(train_loader))
+            val_acc.append(test_accuracy/ len(val_loader))
+            print("Epoch:{}/{}..".format(e+1, epochs),
+                  "Train Loss: {:.3f}..".format(running_loss/len(train_loader)),
+                  "Val Loss: {:.3f}..".format(test_loss/len(val_loader)),
+                  "Train mIoU:{:.3f}..".format(iou_score/len(train_loader)),
+                  "Val mIoU: {:.3f}..".format(val_iou_score/len(val_loader)),
+                  "Train Acc:{:.3f}..".format(accuracy/len(train_loader)),
+                  "Val Acc:{:.3f}..".format(test_accuracy/len(val_loader)))
+        
+    history = {'train_loss' : train_losses, 'val_loss': test_losses,
+               'train_miou' :train_iou, 'val_miou':val_iou,
+               'train_acc' :train_acc, 'val_acc':val_acc}
+    return history
