6514e565d5c2dd5a571a3a92e09e83613908c3f6
unknown <4rthur.arut@gmail.com>
2021-04-25 11:49:16 +0300

    Move uploads folder into static folder

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 from flask import Flask, render_template, url_for, jsonify, request, flash, redirect, send_from_directory
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = './temp'
+UPLOAD_FOLDER = './static/uploads/'
+UPLOAD_NAME = 'image.jpg'
 ALLOWED_EXTENSIONS = {'jpeg', 'jpg', 'png'}
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_NAME'] = UPLOAD_NAME
 
 @app.route('/')
 def upload():
@@ -27,15 +29,14 @@ def upload_file():
         # if f.filename == '':
         #     flash('No selected file')
         #     return redirect(request.url)
-        f.filename = secure_filename(f.filename)
-        # f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-        f.save(f.filename)
+        f.filename = secure_filename(app.config['UPLOAD_NAME'])
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
         # if f and allowed_file(f.filename):
         #     filename = secure_filename(f.filename)
         #     f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             # return redirect(url_for('uploaded_file',
             #                         filename=filename))
-    return f.filename
+    return render_template('result.html')
 
 # @app.route('/temp/<filename>')
 # def uploaded_file(filename):
