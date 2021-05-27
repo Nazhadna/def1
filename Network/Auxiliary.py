0c6c5b52499076512464e9ff23bbb50c39475db7
trkdmtr <trkdmtr@gmail.com>
2021-05-27 14:56:09 +0300

    Added loader for Pipes dataset

--- Network/Auxiliary.py
+++ Network/Auxiliary.py
@@ -22,3 +22,21 @@ def tensor_to_pillow(t):
     t1 = t1*255
     img = Image.fromarray(np.uint8(t1), mode)
     return img 
+    
+    
+def flip(arr):
+    l = len(arr)
+    h = l//2
+    for i in range(h):
+        arr[i],arr[l-i-1] = arr[l-i-1].copy(),arr[i].copy()
+        
+        
+def two_for_one(IMG_PATH,MASK_PATH):
+    for idx in df['id']:
+        name = 'ex' + str(int(idx[2:])+45)
+        img = Image.open(IMG_PATH+idx+'.jpg')
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+        img.save(IMG_PATH+'flip\\'+name+'.jpg')
+        mask = np.load(MASK_PATH+idx+'.npy')
+        flip(mask)
+        np.save(MASK_PATH+'flip\\'+name+'.npy',mask)
